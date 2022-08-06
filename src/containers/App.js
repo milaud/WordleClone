a5299e28d98a21c0540531ca323c731882f43cb8
Milaud Nik-Ahd <nmilaud@gmail.com>
2022-08-06 15:49:51 -0700

    forgot this

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,34 +1,33 @@
-import BoardContainer from '../components/BoardContainer';
-import KeyboardContainer from '../components/KeyboardContainer';
-import Message from '../components/Message'
-import { words } from '../components/Words'
-import { commonWords } from '../components/CommonWords'
+import GameBoard from '../components/GameBoard/GameBoard'
+import { words } from '../components/Data/Words'
+import { commonWords } from '../components/Data/CommonWords'
 import React from 'react';
 
 class App extends React.Component {
-  alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-  wordList = words.map(word => {
-    return word.toUpperCase()
-  })
-
-  commonWordList = commonWords.map(word => {
-    return word.toUpperCase()
-  })
 
   constructor(props) {
     super(props)
     let word = this.getWord()
+    console.log(word)
 
+    /*
     this.state = {
-      roundOver: false,
-      roundWon: false,
-      currentGuess: "",
-      currentRow: 0,
-      currentGuesses: [],
+      roundOver: false, // should be here
+      roundWon: false, // should be here
+      currentGuess: "", // passed up from board
+      currentRow: 0, // passed up from board
+      currentGuesses: [], // should be  here
+      wordToGuess: word, // here, passed down to board
+      message: "", // passed up from board
+      previousGameInfo: [] // here
+    }
+    */
+    this.state = {
       wordToGuess: word,
-      message: "",
-      previousGameInfo: []
+      roundOver: false, // should be here
+      roundWon: false, // should be here
+      currentGuesses: [], // should be  here
+      previousGameInfo: [] // here
     }
     /*
     // previousGameInfo holds list that contains all previous words/guesses
@@ -40,13 +39,48 @@ class App extends React.Component {
     */
   }
 
+  startNewGame() {
+    let newWord = this.selectNewWord()
+    console.log(newWord)
+    /*
+    let previousGameInfo = {
+      word: this.state.wordToGuess, 
+      currentGuesses: this.state.currentGuesses,
+      roundWon: this.state.roundWon
+    }
+    */
+
+    /*
+    this.setState(() => ({
+      roundOver: false,
+      roundWon: false,
+      wordToGuess: newWord,
+      message: "",
+      currentGuess: "",
+      currentRow: 0,
+      currentGuesses: [],
+      previousGameInfo: [...this.state.previousGameInfo, previousGameInfo]
+    }))
+    */
+
+    this.setState(() => ({
+      roundOver: false,
+      roundWon: false,
+      wordToGuess: newWord,
+      message: "", // in gameboard
+      currentGuess: "", // in gameboard
+      currentRow: 0, // in gameboard
+      currentGuesses: [], // in gameboard
+    }))
+  }
+
   getWord() {
     var foundWord = false;
     var randomWord;
     while (!foundWord) {
-      var randomIndex = Math.floor(Math.random() * this.commonWordList.length);
-      randomWord = this.commonWordList[randomIndex];
-      if (this.wordList.includes(randomWord)) {
+      var randomIndex = Math.floor(Math.random() * commonWords.length);
+      randomWord = commonWords[randomIndex];
+      if (words.includes(randomWord)) {
         foundWord = true
       }
     }
@@ -65,10 +99,10 @@ class App extends React.Component {
       //console.log(previousWords)
   
       do {
-        randomIndex = Math.floor(Math.random() * this.commonWordList.length);
-        randomWord = this.commonWordList[randomIndex];
+        randomIndex = Math.floor(Math.random() * commonWords.length);
+        randomWord = commonWords[randomIndex];
   
-        if (this.wordList.includes(randomWord) && !previousWords.includes(randomWord)) {
+        if (words.includes(randomWord) && !previousWords.includes(randomWord)) {
           foundUniqueWord = true
         } else {
           //console.log(`Found duplicate word ${randomWord} in list`)
@@ -76,163 +110,42 @@ class App extends React.Component {
   
       } while(!foundUniqueWord)
     } else {
-      randomIndex = Math.floor(Math.random() * this.commonWordList.length);
-      randomWord = this.commonWordList[randomIndex];
+      randomIndex = Math.floor(Math.random() * commonWords.length);
+      randomWord = commonWords[randomIndex];
     }
 
     return randomWord
   }
 
-  writeToTile(key) {
-    // add it to first available tile
-    if (this.state.currentGuess.toUpperCase().length < 5) {
-      this.setState(prevState => ({
-        currentGuess: prevState.currentGuess + key
-      }))
-    }
-  }
-  
-  isfRowIsFilled() {
-    return this.state.currentGuess.length === 5 ? true : false
-  }
-
-  isValidGuess() {
-    var validWord = this.wordList.includes(this.state.currentGuess.toUpperCase())
+  isValidGuess(currentGuess) {
+    var validWord = words.includes(currentGuess.toUpperCase())
     if (!validWord) {
       return false
     }
     return true
   }
 
-  doesGuessMatchWord() {
-    // check if guess matches word
-    let currentGuess = this.state.currentGuess
-    let wordToGuess = this.state.wordToGuess
-
-    if (currentGuess === wordToGuess) {
-      return true
-    }
-    return false
-  }
-
-  writeRow() {
-    this.setState(prevState => ({
-      currentGuess: "",
-      currentRow: prevState.currentRow + 1,
-      currentGuesses: [...this.state.currentGuesses, prevState.currentGuess]
-    }))
-  }
-
-  submitGuess() {
-    if (!this.isfRowIsFilled()) {
-      this.updateMessage("Not enough letters")
-      return
-    } 
-    if (!this.isValidGuess()) {
-      this.updateMessage("Not a valid word")
-      return
-    }
-    
-    if (this.doesGuessMatchWord()) {
-      this.setState(prevState => ({
-        roundOver: true,
-        roundWon: true,
-        message: "Congrats!"
-      }))
-    } else if (this.state.currentRow === 5 && this.state.currentGuesses[-1] !== this.state.wordToGuess) {
-      this.setState(prevState => ({
-        roundOver: true,
-        message: this.state.wordToGuess
-      }))
-    }
-
-    this.writeRow()
-  }
-
-  eraseLatestTile() {
-    // remove from latest written tile
-    if (this.state.currentGuess.toUpperCase().length > 0) {
-      this.setState(prevState => ({
-        currentGuess: prevState.currentGuess.slice(0, prevState.currentGuess.length - 1)
-      }))
-    }
-  }
-
-  onLetterChange = (key) => {
-    //console.log(key)
-
-    if (!this.state.roundOver && this.state.currentRow < 6) {
-      this.updateMessage("")
-
-      // key pressed is a letter
-      if (this.alphabet.indexOf(key) !== -1) {
-        this.writeToTile(key)
-      } else if (key === "⌫" || key === "BACKSPACE" || key === "DELETE") {
-        this.eraseLatestTile()
-      } else if (key === "⏎" || key === "ENTER") {
-        this.submitGuess()
-      }
-    }
-  }
-
-  startNewGame() {
-    let newWord = this.selectNewWord()
-    let previousGameInfo = {
-      word: this.state.wordToGuess, 
-      currentGuesses: this.state.currentGuesses,
-      roundWon: this.state.roundWon
-   }
-
-    this.setState(() => ({
-      roundOver: false,
-      roundWon: false,
-      wordToGuess: newWord,
-      message: "",
-      currentGuess: "",
-      currentRow: 0,
-      currentGuesses: [],
-      previousGameInfo: [...this.state.previousGameInfo, previousGameInfo]
-    }))
-  }
-
-  handleKeyBoard = (event) => {
-    let key = event.key.toUpperCase()
-    this.onLetterChange(key)
-  }
-
-  updateMessage(message) {
-    this.setState(() => ({
-      message: message
-    }))
-  }
-
-
   render() {
-    //console.log(this.state)
+    console.log("app.js state:")
+    console.log(this.state)
+    console.log("\n\n")
     return (
       <div className="App" >
         <div className="navbar">
-        <label class="switch">
+        <label className="switch">
           <input type="checkbox"></input>
-          <span class="slider round"></span>
+          <span className="slider round"></span>
         </label>
           <a href=''>(Not) Wordle</a>
           <button className="newGameButton" onClick={this.startNewGame.bind(this)}>New Word</button>
         </div>
-        <div className='game' tabIndex={1} onKeyDown={this.handleKeyBoard}>
-            {/* <BoardContainer state={this.state}/> */}
-            < Message message={this.state.message} />
-            <BoardContainer 
-              currentGuess={this.state.currentGuess}
-              currentGuesses={this.state.currentGuesses}
-              currentRow={this.state.currentRow}
-              wordToGuess={this.state.wordToGuess}
-            />
-            <KeyboardContainer 
-              currentGuess={this.state.currentGuess} 
-              onLetterChange={this.onLetterChange}
-            />
-        </div>
+        <GameBoard 
+          wordToGuess={this.word}
+          roundOver={this.roundOver}
+          roundWon={this.roundWon}
+          currentGuesses={this.currentGuesses}
+          isValidGuess={this.isValidGuess}
+        />
       </div>
     );
   }
